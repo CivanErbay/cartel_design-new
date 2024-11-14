adbf027eb7f80b9e7493d9928b94b70e863dd524
Paco Muro <paco@studioborabora.com>
2024-11-14 17:10:00 +0100

    logo link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex justify-between items-center py-8">
-    <h2 class="font-kodchasan text-2xl">Cartel Design</h2>
+    <NuxtLink to="/">
+      <h2 class="font-kodchasan text-2xl">Cartel Design</h2>
+    </NuxtLink>
     <ul class="flex items-center gap-10">
       <li v-for="link in links">
         <a :href="link.url">{{ link.title }}</a>
