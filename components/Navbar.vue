b1b539078bd7bb9f4ea454166d2f0e77fc11ecf1
Civan Erbay <civan.erbay@web.de>
2024-11-15 10:49:24 +0100

    Finish Datenschutz and Impressm content and styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex justify-between items-center py-8 px-5 lg:px-0">
-    <h2 class="font-kodchasan text-2xl">Cartel Design</h2>
+    <NuxtLink to="/">
+      <h2 class="font-kodchasan text-2xl">Cartel Design</h2>
+    </NuxtLink>
     <ul class="lg:flex items-center gap-10 hidden">
       <li v-for="link in links">
         <a :href="link.url">{{ link.title }}</a>
@@ -11,25 +13,13 @@
 
       </div>
     </button> -->
-    <button
-      class="w-fit rounded-lg bg-gradient-to-r from-fuchsia-800 to-sky-600 p-0.5"
-    >
-      <div
-        class="flex h-full rounded-lg w-full px-4 py-2 items-center justify-center bg-gray-800"
-      >
+    <button class="w-fit rounded-lg bg-gradient-to-r from-fuchsia-800 to-sky-600 p-0.5">
+      <div class="flex h-full rounded-lg w-full px-4 py-2 items-center justify-center bg-gray-800">
         Jetzt kontaktieren
-        <svg
-          class="ml-2"
-          width="18"
-          height="15"
-          viewBox="0 0 18 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg class="ml-2" width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M15.3638 7.92447L9.96439 13.3239C9.72031 13.568 9.72031 13.9637 9.96439 14.2078C10.2085 14.4518 10.6042 14.4518 10.8483 14.2078L17.3134 7.74263C17.5575 7.49856 17.5575 7.10284 17.3134 6.85875L10.6895 0.183057C10.4454 -0.061019 10.0497 -0.061019 9.80561 0.183057C9.56153 0.427133 9.56153 0.822864 9.80561 1.06694L15.3614 6.67448L0.919392 6.67511C0.574207 6.67511 0.294388 6.95493 0.294388 7.30011C0.294401 7.64528 0.574221 7.9251 0.919392 7.92512L15.3638 7.92447Z"
-            fill="#FAFAFA"
-          />
+            fill="#FAFAFA" />
         </svg>
       </div>
     </button>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,57 +1,63 @@
 <template>
-  <div class="mt-6 text-sm">
-    <p>Angaben gemäß § 5 TMG</p>
-    <p>Sömmeringstr. 73</p>
-    <p>50825 Köln</p>
-    <h4 class="font-semibold mt-3">Kontakt</h4>
-    <p>hello@cartel-design.com</p>
-    <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</p>
-    <h4 class="font-semibold mt-3">Erbay & Muro Webdevelopment GbR</h4>
-  </div>
-  <div>
+  <div class="mt-6 text-sm px-5 lg:px-0">
+    <h6 class="font-semibold text-xl mb-6">Impressum</h6>
+    <span>Angaben gemäß § 5 TMG</span>
+    <span> Muro & Erbay Webdevelospanment GbR / Cartel Design
+      Sömmeringstr. 73
+      50823 Köln
+      Deutschland
+      E-Mail: hello@cartel-design.com
+      Website: www.cartel-design.com</span>
+    <h6 class="font-semibold mt-3">Kontakt</h6>
+    <span>hello@cartel-design.com</span>
+    <br>
+    <br>
+    <span>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</span>
+    <h6 class="font-semibold mt-3">Muro & Erbay Webdevelopment GbR</h6>
+
     <h5 class="font-semibold mt-6">EU-Streitschlichtung</h5>
-    <p>
+    <span class="text-justify text-base">
       Die Europäische Kommission stellt eine Plattform zur
-      Online-Streitbeilegung (OS) bereit: https://ec.europa.eu/consumers/odr.
+      Online-Streitbeilegung (OS) bereit: httpns://ec.europa.eu/consumers/odr.
       Unsere E-Mail-Adresse finden Sie oben im Impressum.
-    </p>
+    </span>
     <h5 class="font-semibold mt-6">Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h5>
-    <p>
+    <span class="text-justify text-base">
       Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor
       einer Verbraucherschlichtungsstelle teilzunehmen.
-    </p>
+    </span>
 
     <h5 class="font-semibold mt-6">Haftung für Inhalte</h5>
-    <p>
+    <span class="text-justify text-base">
       Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
       diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis
       10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
       übermittelte oder gespeicherte fremde Informationen zu überwachen oder
       nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
-      hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+      hinweisen. Verpflichtungen zur Entfernung oder Spnerrung der Nutzung von
       Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
       Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
       einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
       entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
       entfernen.
-    </p>
+    </span>
 
     <h5 class="font-semibold mt-6">Haftung für Links</h5>
-    <p>
+    <span class="text-justify text-base">
       Unser Angebot enthält Links zu externen Websites Dritter, auf deren
       Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
       Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
       Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
       verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung
       auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
-      Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+      Zeitpnunkt der Verlinkung nicht erkennbar. Eine pnermanente inhaltliche
       Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte
       einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
       Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-    </p>
+    </span>
 
     <h5 class="font-semibold mt-6">Urheberrecht</h5>
-    <p>
+    <span class="text-justify text-base">
       Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
       Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
       Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen
@@ -64,28 +70,6 @@
       aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
       Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend
       entfernen.
-    </p>
+    </span>
   </div>
 </template>
-
-<style>
-.landing-grid {
-  background-size: 100px 100px;
-  background-image:
-    linear-gradient(to right, #6c63ff 1px, transparent 1px),
-    linear-gradient(to bottom, #6c63ff 1px, transparent 1px);
-}
-
-.dark {
-  .landing-grid {
-    background-image:
-      linear-gradient(to right, #6c63ff 1px, transparent 1px),
-      linear-gradient(to bottom, #6c63ff 1px, transparent 1px);
-  }
-}
-
-.reverse div div {
-  grid-auto-flow: dense;
-  direction: rtl;
-}
-</style>
