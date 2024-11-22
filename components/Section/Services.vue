50fc717bd3a6c8828093ed2919d28f061ab10da4
Paco Muro <paco@studioborabora.com>
2024-11-22 13:44:12 +0100

    side floating text

--- components/Section/Services.vue
+++ components/Section/Services.vue
@@ -1,65 +1,67 @@
 <template>
   <div class="relative">
-    <DefaultGrid class="mt-24 w-full min-h-screen">
+    <DefaultGrid class="mt-24 w-full min-h-screen overflow-hidden">
       <div class="lg:col-start-1 lg:col-end-12 relative">
         <div class="w-full h-full flex flex-col items-center justify-center">
-          <h2
-            class="text-primary uppercase text-8xl font-semibold mb-4"
-            v-for="(label, i) in labels"
-            :key="i"
-          >
-            {{ label }}
-          </h2>
+          <div class="relative group" v-for="(service, i) in services" :key="i">
+            <h2 class="text-primary uppercase text-8xl font-semibold mb-4">
+              {{ service.label }}
+            </h2>
+            <div
+              class="absolute top-0 h-full flex items-center mr-8 text-secondary text-lg w-max right-full opacity-0 group-hover:opacity-100 transition-all"
+            >
+              {{ service.floatingTextLeft }}
+            </div>
+            <div
+              class="absolute top-0 h-full flex items-center ml-8 text-secondary text-lg w-max left-full opacity-0 group-hover:opacity-100 transition-all"
+            >
+              {{ service.floatingTextRight }}
+            </div>
+          </div>
         </div>
       </div>
     </DefaultGrid>
-    <div class="absolute inset-0 pointer-events-none">
-      <div
-        v-for="(text, index) in floatingText"
-        :key="index"
-        :style="{
-          ...getRandomPosition(),
-          transform: `translate(-50%, -50%)`,
-        }"
-        class="absolute text-secondary text-lg w-max"
-      >
-        {{ text }}
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
-const labels = [
-  'ux/ui design',
-  'web apps',
-  'seo',
-  'content',
-  'CMS',
-  'automation',
-  'templates',
-];
-
-const getRandomPosition = () => {
-  const left =
-    Math.random() < 0.5
-      ? `${Math.random() * 30}%`
-      : `${70 + Math.random() * 30}%`;
-  return {
-    top: `${Math.random() * 80 + 10}%`,
-    left,
-  };
-};
-
-const floatingText = [
-  'Was ist das?',
-  'Was bringt mir das?',
-  'Hä, aber wie dann?',
-  'Oh man das ist aber teuer!',
-  'Was ist das?',
-  'Was bringt mir das?',
-  'Hä, aber wie dann?',
-  'Oh man das ist aber teuer!',
+const services = [
+  {
+    label: 'ux/ui design',
+    floatingTextLeft: 'Das kann auch mein Kind',
+    floatingTextRight: 'Ne brauch ich nicht',
+  },
+  {
+    label: 'web apps',
+    floatingTextLeft: 'Es reicht doch mit Instagram',
+    floatingTextRight: 'Ich bin schon bei EbayKleinanzeigen',
+  },
+  {
+    label: 'seo',
+    floatingTextLeft: 'Was ist das denn?',
+    floatingTextRight: 'Was bringt mir das?',
+  },
+  {
+    label: 'content',
+    floatingTextLeft: 'Ich bin doch kein Schriftsteller',
+    floatingTextRight:
+      'Ich habe ja immer Zeit, meine Dienstleistung nochmal zu erklären',
+  },
+  {
+    label: 'CMS',
+    floatingTextLeft: 'Und wie ergänze ich meinen eigenes Website?',
+    floatingTextRight: 'Ja es passt so änderungen brauche ich nicht',
+  },
+  {
+    label: 'automation',
+    floatingTextLeft: 'Kein Problem ich habe doch viel Zeit!',
+    floatingTextRight: 'Papierkram ist doch schön',
+  },
+  {
+    label: 'templates',
+    floatingTextLeft: 'Was wenn ich das nicht kann?',
+    floatingTextRight: 'Ich will aber mein Content anders',
+  },
 ];
 </script>
 
