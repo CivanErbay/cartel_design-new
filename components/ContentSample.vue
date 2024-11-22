af9fa54cb9b821f7a8d270090a904f75c537fe02
Paco Muro <paco@studioborabora.com>
2024-11-22 14:58:01 +0100

    colors

--- components/ContentSample.vue
+++ components/ContentSample.vue
@@ -1,6 +1,6 @@
 <template>
   <DefaultGrid class="py-16">
-    <div class="flex flex-col lg:col-start-3 lg:col-end-8">
+    <div class="flex flex-col lg:col-start-3 lg:col-end-9">
       <div class="mb-4 lg:mb-12">
         <h3 class="text-xl lg:text-5xl font-bold mb-2">
           Design trifft Funktionalität
@@ -16,7 +16,7 @@
         innovative und nachhaltige Ergebnisse zu erzielen.
       </p>
     </div>
-    <div class="flex flex-col lg:col-start-9 lg:col-end-11">
+    <div class="flex flex-col lg:col-start-10 lg:col-end-12">
       <h3 class="mb-8 uppercase text-2xl">Darum geht's</h3>
       <p>UI/UX</p>
       <p>Templating</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
         '1.5xl': '1520px',
         '2xl': '1920px',
       },
-      colors: { primary: '#34c2a7' },
+      colors: { primary: '#34c2a7', secondary: '#99ACD6' },
     },
   },
   variants: {
